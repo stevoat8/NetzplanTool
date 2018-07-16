e88e094755a228853aa0efd168a9a032347fa4cf
SH <shardacker@gmail.com>
2018-07-16 11:27:28 +0200

    testProjekt hinzugefügt

---
+++ NetzplanTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetzplanTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetzplanTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35c4a5f0-7875-4df9-92a8-af442f3e94e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NetzplanTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Netzplan;
+
+namespace NetzplanTest
+{
+    [TestClass]
+    public class NetzplanTest
+    {
+        [TestMethod]
+        public void TestForwardScheduling()
+        {
+            
+        }
+    }
+}
