7642d6cc7b62045ee50cfcb3ac66e20c22b43a17
SH <shardacker@gmail.com>
2018-07-19 15:33:37 +0200

    fix

--- NetzplanTool/Program.cs
+++ NetzplanTool/Program.cs
@@ -177,7 +177,6 @@ namespace NetzplanTool
             /// <summary>
             /// Dateiformat in welches der Graph gespeichert werden soll (jpg, png, svg, pdf, plain, plainext).
             /// </summary>
-            Enums.GraphReturnType OutputFileFormat { get; set; }
 
             /// <summary>
             /// Speicherpfad unter dem der erstellte Graph gespeichert wird.
